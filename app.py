2423a62e26fadd65e4e86c566f821337dee5429c
Радислав <mistral2798@mail.ru>
2025-07-05 01:53:00 +0300

    Добавил еще одну библиотеку в req

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 
-rest = pd.read_excel('rest.xlsx')
+rest = pd.read_excel('rest.xlsx', engine="openpyxl")
 rest['Итог'] = rest['Итог'].round(1)
 rest['Дата посещения'] = rest['Дата посещения'].dt.to_period('D')
 st.title('Витрина посещенных ресторанов')
